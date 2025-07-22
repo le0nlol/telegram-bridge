a352aede16ecf94bcad944a7da5bd618cb1d963b
Romawertq <plitynplitin10@gmail.com>
2025-07-22 14:16:18 +0300

    Начало жизни

---
+++ src/main/java/com/Romawertq/telegramBridge/TelegramBridge.java
@@ -0,0 +1,17 @@
+package com.Romawertq.telegramBridge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TelegramBridge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Hellow World!");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
