0e01c8f08a7fd86eeef4731d42fb9536d227a237
Romawertq <plitynplitin10@gmail.com>
2025-07-22 15:30:47 +0300

    Привет мир

---
+++ telegram-bridge/src/main/java/com/romawertq/telegrambridge/Telegram_bridge.java
@@ -0,0 +1,17 @@
+package com.romawertq.telegrambridge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Telegram_bridge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Hello world!");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
