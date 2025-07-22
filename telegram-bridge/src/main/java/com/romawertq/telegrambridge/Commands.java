cc7c2286f2928268e5deb017015177f7e87d247a
Romawertq <plitynplitin10@gmail.com>
2025-07-22 16:37:52 +0300

    Добавление ивента

---
+++ telegram-bridge/src/main/java/com/romawertq/telegrambridge/Commands.java
@@ -0,0 +1,28 @@
+package com.romawertq.telegrambridge;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class Commands implements CommandExecutor, Listener {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(!(sender instanceof Player)){
+            return false;
+        }
+        Player player = (Player) sender;
+
+        return true;
+    }
+    @EventHandler
+    public void playerjoin(PlayerJoinEvent event){
+        Bukkit.broadcastMessage(event.getPlayer().getDisplayName() + "зашёл на сервер");
+    }
+}
--- telegram-bridge/src/main/java/com/romawertq/telegrambridge/Telegram_bridge.java
+++ telegram-bridge/src/main/java/com/romawertq/telegrambridge/Telegram_bridge.java
@@ -1,13 +1,18 @@
 package com.romawertq.telegrambridge;
 
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Telegram_bridge extends JavaPlugin {
+public final class Telegram_bridge extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         System.out.println("Hello world!");
-
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents (new Commands(), this);
     }
 
     @Override
