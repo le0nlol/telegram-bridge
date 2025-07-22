42703e8844affe0e060639bb54f5012961eb00c3
Romawertq <plitynplitin10@gmail.com>
2025-07-22 22:27:24 +0300

    Практический запуск бота

--- telegram connect/src/main/java/com/romawertq/telegrambridge/Telegram_bridge.java
+++ telegram connect/src/main/java/com/romawertq/telegrambridge/Telegram_bridge.java
@@ -2,19 +2,19 @@ package com.romawertq.telegrambridge;
 
 
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public final class Telegram_bridge extends JavaPlugin implements Listener {
-
+    private String botToken;
+    private String chatId;
+    private int topicId;
     @Override
     public void onEnable() {
+        saveDefaultConfig();
         System.out.println("Hello world!");
-        PluginManager pm = getServer().getPluginManager();
-        pm.registerEvents (new Commands(), this);
         TelegramBotsApi tgapi = null;
         try {
             tgapi = new TelegramBotsApi(DefaultBotSession.class);
@@ -26,6 +26,17 @@ public final class Telegram_bridge extends JavaPlugin implements Listener {
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
+
+        botToken = getConfig().getString("bot.token");
+        chatId = getConfig().getString("bot.chat-id");
+        topicId = getConfig().getInt("bot.topic-id");
+
+    }
+
+    public static void main(String[] args) {
+        botik a = new botik();
+        String value = a.getData();
+        System.out.println(value); // Вывод значения: "Значение из ClassB"
     }
 
     @Override
--- telegram connect/src/main/java/com/romawertq/telegrambridge/bot.java
+++ telegram connect/src/main/java/com/romawertq/telegrambridge/bot.java
@@ -1,19 +1,33 @@
 package com.romawertq.telegrambridge;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import static org.bukkit.Bukkit.getServer;
 
 public class bot extends TelegramLongPollingBot implements Listener {
+    private String f;
+    private String g;
+    private String k;
+    private String r;
+    private String message;
+    private String NamePlayer;
+    private int receivedValue;
+    public String botToken;
+    public String chatId;
+    public int topicId;
+
+
     @Override
     public void onUpdateReceived(Update update) {
-        PluginManager pm = getServer().getPluginManager();
-        pm.registerEvents(new Commands(), (Plugin) this);
         String chatId = update.getMessage().getChatId().toString();
         String Text = update.getMessage().getText();
         SendMessage sendMessage = new SendMessage();
@@ -28,15 +42,41 @@ public class bot extends TelegramLongPollingBot implements Listener {
         }
     }
 
+
     @Override
     public String getBotUsername() {
-        return "onlineserverle0n_bot";
+        return "";
     }
 
     @Override
     public String getBotToken() {
-        return "7725985929:AAFJsqfj84G0GKQQ2WgL87GXp9ykuraWp7k";
+        return "";
     }
 
+    @EventHandler
+    public void playerjoin(PlayerJoinEvent event){
+        f = event.getPlayer().getDisplayName() + " зашёл на сервер!";
+    }
+    @EventHandler
+    public void playerquit(PlayerQuitEvent event){
+        g = event.getPlayer().getDisplayName() + " вышел с сервера!";
+    }
+    @EventHandler
+    public void playerchat(PlayerChatEvent event){
+        message = event.getMessage();
+        NamePlayer = event.getPlayer().getName();
+    }
+
+    @EventHandler
+    public void serverstart(PluginEnableEvent event){
+        k = "Просыпаемся, сервер запущен!";
+    }
+    @EventHandler
+    public void serverstop(PluginDisableEvent event){
+        r = "Пора спать, сервер тоже спать!";
+    }
+
+}
+class botik extends JavaPlugin {
 
 }
